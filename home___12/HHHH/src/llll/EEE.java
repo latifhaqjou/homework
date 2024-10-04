d15d7f4508d134ab0646a296f2db06f3031392c1
latifhaqjou <haqjoul@gmail.com>
2024-10-04 17:03:57 +0430

    Add files via upload

---
+++ home___12/HHHH/src/llll/EEE.java
@@ -0,0 +1,73 @@
+package llll;
+
+import java.util.Scanner;
+
+public class EEE {
+public static void main(String[] args) {
+	
+	
+////1.	A = π * r*r     area of circle
+//	System.out.print("enter the radius here :   ");
+//	Scanner black=new Scanner(System.in);
+//	double ra=black.nextDouble();
+//	System.out.print("the area of the circle is =  ");
+//	System.out.println(3.1415*ra*ra);
+	
+	
+	
+
+	
+////2.	P = 2 *π*r     perimeter of circle
+//	System.out.print("enter the radius here:");
+//	Scanner green=new Scanner(System.in);
+//	double per=green.nextDouble();
+//	System.out.print("perimeter=  ");
+//	System.out.println(2*3.1415*per);
+	
+	
+	
+	
+////	3. A = (1/2)*b*h    area of triangle
+//	System.out.print("first enter the base and pres "
+//			+"'ENTER KEY' then enter the height");
+//	Scanner kay=new Scanner(System.in);
+//	double b=kay.nextDouble();
+//	double h=kay.nextDouble();
+//	System.out.print("the area of the triangle is=  ");
+//	System.out.println((0.5)*b*h);
+	
+	
+
+	
+//9.   F= ((C*9/5)+32)    تبدیلـدرجهـحرارتـازـسیلسیوسـبهـفارنهایت
+	System.out.println("enter the celsius deegre");
+	Scanner jali= new Scanner(System.in);
+	double celsius=jali.nextDouble();
+	System.out.print("fahrenheit deegre=  ");
+	System.out.println(9/5d *celsius+32d);
+
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
+}
---
+++ home___12/HHHH/src/llll/homwork_latif2.java
@@ -0,0 +1,19 @@
+//package llll;
+//
+//import java.util.Scanner;
+//
+//public class SquareRoot {
+//	
+//	
+////	8.   √(a^2+b^2)    hypotenuse of a right-angled triangle
+//	
+//	public static void main(String[] args) {
+//		System.out.println("enter the sides of the triangle");
+//		Scanner root=new Scanner(System.in);
+//		double a =root.nextDouble();
+//		double b=root.nextDouble();
+//		double Hypotenuse =Math.sqrt((a*a)+(b*b));
+//		System.out.print(" the hypotenuse of triangle is =  ");
+//		System.out.println(Hypotenuse);
+//	}
+//}
